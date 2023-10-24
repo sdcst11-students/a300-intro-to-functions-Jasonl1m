25fe6b6522e09107e57125e13a5af37f4e25943d
Jasonl1m <jasonl694@deltalearns.ca>
2023-10-24 12:56:44 -0700

    complete

--- task1.py
+++ task1.py
@@ -1,6 +1,17 @@
 def A():
+    print("Hello")
 
 def B():
-
+    print("How are you")
 
 def C():
+    print("Hi")
+
+x = input("Enter A or B or C = ")
+
+if x == "A":
+    A()
+elif x == "B":
+    B()
+else:
+    C()
--- task2.py
+++ task2.py
@@ -8,3 +8,21 @@ displays instructions and how to play
 game()
 plays the games
 """
+import random
+def game():
+    for i in range(10):
+        x = input("Guess a number = ")
+        y = random.randint(1,100)
+        if x == y:
+            print("You are correct!")
+            break
+        elif i == 9:
+            print("You have lost :(")
+            break
+        else:
+            print("You are incorrect!")
+
+
+def title():
+    print("You have 10 tries to enter a number between 1 and 100, if you guess correct you win! Good luck!")
+title()
